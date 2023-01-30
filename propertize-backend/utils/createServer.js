b5c913bc5ce66db3f83181f20508cf9a6534f9ad
dukelester <dukelester4@gmail.com>
2023-01-30 09:04:35 +0300

    added the middleware for cookies and the json

--- propertize-backend/utils/createServer.js
+++ propertize-backend/utils/createServer.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const createServer = () => {
   const app = express();
+  app.use(express.json());
+  app.use(cookieParser());
   return app;
 };
 
