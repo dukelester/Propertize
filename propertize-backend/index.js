5c8188836bbf71cb98d3abdcc1c826acf71bd437
dukelester <dukelester4@gmail.com>
2023-01-30 09:43:03 +0300

    Database connection
    connected to the mongo database. Here we send signals for both connection and database error.

--- propertize-backend/index.js
+++ propertize-backend/index.js
@@ -2,11 +2,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 import createServer from './utils/createServer.js';
 
 dotenv.config();
 
 const app = createServer();
+mongoose.connection.on('connected', () => {
+  console.log('database connected successfully');
+});
+mongoose.connection.on('disconnected', async (error) => {
+  if (error) {
+    console.log('Database has been Disconnected!');
+    throw new Error();
+  }
+  await mongoose.connection.close();
+});
+
+const dbConnect = async () => {
+  await mongoose.connect(process.env.MONGO);
+};
 
 const port = process.env.PORT;
 
@@ -14,6 +30,7 @@ app.listen(port, (error) => {
   if (error) {
     throw new Error();
   }
+  dbConnect();
   console.log(`The server is running at http://localhost:${port}`);
 });
 
