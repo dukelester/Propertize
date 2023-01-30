90b16da978f2882d672d8d92dd3f0c0459f433d4
dukelester <dukelester4@gmail.com>
2023-01-30 23:19:54 +0300

    added more controllers for the contact us to enable crud

--- propertize-backend/controllers/contactus.js
+++ propertize-backend/controllers/contactus.js
@@ -1,6 +1,16 @@
 /* eslint-disable no-unused-vars */
-const contactUs = (req, res, next) => {
+export const contactUs = (req, res, next) => {
   res.send('contact us');
 };
 
-export default contactUs;
+export const allContacts = (req, res, next) => {
+  res.send({ message: 'All the contact us messages' });
+};
+
+export const contactDetails = (req, res, next) => {
+  res.send({ message: 'Info about the contact us' });
+};
+
+export const deleteContact = (req, res, next) => {
+  res.send({ message: 'Deleet contact' });
+};
