d6216b99a695b6bef8ab73d9e430f20683157a5b
dukelester <dukelester4@gmail.com>
2023-01-30 10:59:06 +0300

    Added Property Model
    The property model for the property details.

---
+++ propertize-backend/models/Property.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const PropertySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  amenities: {
+    type: [String],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  additionalDetails: {
+    type: [String],
+  },
+  propertyType: {
+    type: String,
+  },
+  details: {
+    type: [String],
+  },
+  attachments: {
+    type: [String],
+  },
+  latitude: {
+    type: String,
+  },
+  longitude: {
+    type: String,
+  },
+  floorPlans: {
+    type: [String],
+  },
+  propertyFor: {
+    type: String, // rent, buy, build
+  },
+});
+
+export default mongoose.model('Property', PropertySchema);
