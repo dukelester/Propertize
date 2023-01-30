1146dafa3e6fe74488ea20444ea04a6e58846309
dukelester <dukelester4@gmail.com>
2023-01-30 23:41:21 +0300

    Added the controllers for password reset and password reset confirm to allow the users to request password reset.

--- propertize-backend/controllers/auth.js
+++ propertize-backend/controllers/auth.js
@@ -6,3 +6,11 @@ export const userRegister = (req, res, next) => {
 export const userLogin = (req, res, next) => {
   res.send({ message: 'user login' });
 };
+
+export const passwordReset = (req, res, next) => {
+  res.send({ message: 'user password reset' });
+};
+
+export const passwordResetConfirm = (req, res, next) => {
+  res.send({ message: 'user password reset confirmation' });
+};
