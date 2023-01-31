803a04e6173df7981983a895e97348dd5b75dbc2
dukelester <dukelester4@gmail.com>
2023-01-31 08:23:30 +0300

    Added the error middleware
    it takes 4 parameters and error is the first one.

--- propertize-backend/index.js
+++ propertize-backend/index.js
@@ -29,6 +29,17 @@ const dbConnect = () => {
   }
 };
 
+app.use((error, req, res, next) => {
+  const errorStatus = error.status || 500;
+  const errorMessage = error.message || ' Something went wrong!';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: error.stack,
+  });
+});
+
 app.listen(port, (error) => {
   if (error) {
     throw new Error();
