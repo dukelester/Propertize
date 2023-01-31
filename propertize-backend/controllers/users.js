fdd6d9b1e5629286bb27c03024f02257dcf421fc
dukelester <dukelester4@gmail.com>
2023-01-31 17:26:52 +0300

    Added the User CRUD operations to the controllers. This implements the endpoints logic

--- propertize-backend/controllers/users.js
+++ propertize-backend/controllers/users.js
@@ -1,17 +1,44 @@
-/* eslint-disable no-unused-vars */
+import User from '../models/User.js'
 
-export const getAllUsers = (req, res, next) => {
-  res.send({ message: 'All Users' });
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const allUsers = await User.find();
+    res.status(200).json(allUsers);
+  } catch (error) {
+      next(error);
+  };
 };
 
-export const getUserById = (req, res, next) => {
-  res.send({ message: 'get User details' });
+export const getUserById = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const foundUser = await User.findById(userId);
+    res.status(200).json(foundUser);
+  } catch (error) {
+      next(error);
+  };
 };
 
-export const updateUser = (req, res, next) => {
-  res.send({ message: 'update the User details' });
+export const updateUser = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(userId, { $set: req.body }, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+      next(error);
+  };
 };
 
-export const deleteUser = (req, res, next) => {
-  res.send({ message: 'delete the User details' });
+export const deleteUser = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    await User.findByIdAndDelete(userId);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: `User with the id ${userId} has been deleted sucessfully!`,
+    })
+  } catch (error) {
+      next(error);
+  };
 };
