60209570e2e26df36b13a4502ee0f18c5f5bfb6c
dukelester <dukelester4@gmail.com>
2023-01-31 13:00:45 +0300

    Testing the contact-us routes for all contacts and getting specific contact us

--- propertize-backend/tests/contactus.spec.js
+++ propertize-backend/tests/contactus.spec.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
+import request from 'supertest';
+import createServer from '../utils/createServer.js';
+
+const app = createServer();
 
 dotenv.config();
 
@@ -12,11 +16,28 @@ afterEach(async () => {
 });
 
 describe('Contact us route', () => {
-  describe('getting all contacts /', () => {
+  describe('getting all contacts /api/contact-us', () => {
     it('should return a list of contacts with 200 status code', async () => {
-        console.log('testing ....')
+        const res = await request(app).get('/api/contact-us');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBeGreaterThan(1)
     });
-    
-  })
+  });
+  describe('getting a specific contact /api/contact-us/find/:contactId', () => {
+    it('should return a specific contact message with a status 200 ok', async () => {
+        const contactId = '63d8a1282ce18a93be4fc4b4';
+        const res = await request(app).get(`/api/contact-us/find/${contactId}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body._id).toStrictEqual(contactId);
+    });
+  });
+  describe('getting a specific contact /api/contact-us/find/:contactId', () => {
+    it('should return an error message for not found contact', async () => {
+        const contactId = '63d8a1282ce18a93fc4b4';
+        const res = await request(app).get(`/api/contact-us/find/${contactId}`);
+        expect(res.statusCode).toBe(500);
+    });
+
+  });
 
 })
