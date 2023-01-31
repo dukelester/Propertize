368369478cd10071f69cf6a2fb9e5df8598ba02a
dukelester <dukelester4@gmail.com>
2023-01-31 08:09:33 +0300

    Added the Post method for creating a contact message
    the client sends a name, email, phone number, message, category and the subject about the issue or inquiry

--- propertize-backend/controllers/contactus.js
+++ propertize-backend/controllers/contactus.js
@@ -1,6 +1,19 @@
+/* eslint-disable import/extensions */
+import Contact from '../models/Contact.js';
+
 /* eslint-disable no-unused-vars */
-export const contactUs = (req, res, next) => {
-  res.send('contact us');
+export const contactUs = async (req, res, next) => {
+  try {
+    if (req.body) {
+      const newContact = await Contact.create(req.body);
+      const savedContact = await newContact.save();
+      res.status(201).json(savedContact);
+    } else {
+      res.status(400).json({ message: ' Bad information format!' });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const allContacts = (req, res, next) => {
