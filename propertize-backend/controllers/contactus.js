3c7c13a77e38756789e60df3ecac7b25540b73fc
dukelester <dukelester4@gmail.com>
2023-01-31 08:48:39 +0300

    Added the get all contact us endpoint to return a list of amm availabe contacts

--- propertize-backend/controllers/contactus.js
+++ propertize-backend/controllers/contactus.js
@@ -4,20 +4,21 @@ import Contact from '../models/Contact.js';
 /* eslint-disable no-unused-vars */
 export const contactUs = async (req, res, next) => {
   try {
-    if (req.body) {
-      const newContact = await Contact.create(req.body);
-      const savedContact = await newContact.save();
-      res.status(201).json(savedContact);
-    } else {
-      res.status(400).json({ message: ' Bad information format!' });
-    }
+    const newContact = await Contact.create(req.body);
+    const savedContact = await newContact.save();
+    res.status(201).json(savedContact);
   } catch (error) {
     next(error);
   }
 };
 
-export const allContacts = (req, res, next) => {
-  res.send({ message: 'All the contact us messages' });
+export const allContacts = async (req, res, next) => {
+  try {
+    const listOfContacts = await Contact.find();
+    res.status(200).json(listOfContacts);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const contactDetails = (req, res, next) => {
