9a4202654e9111e9d773fb6ac758b9396201f83c
dukelester <dukelester4@gmail.com>
2023-02-02 00:41:53 +0300

    User registration
    added the user registration and allowed user to register and password stored as encrypted.

--- propertize-backend/controllers/auth.js
+++ propertize-backend/controllers/auth.js
@@ -1,6 +1,38 @@
+/* eslint-disable import/extensions */
+import User from '../models/User.js';
+import { hashUserPassword } from '../utils/passwordHashing.js';
+
 /* eslint-disable no-unused-vars */
-export const userRegister = (req, res, next) => {
-  res.send({ message: 'user registration' });
+export const userRegister = async (req, res, next) => {
+  try {
+    const {
+      username, email, phone, firstName,
+      latsName, about, photo, socialLinks, password,
+    } = req.body;
+    if (username && email && phone && password) {
+      const newUser = new User({
+        username,
+        email,
+        phone,
+        firstName,
+        latsName,
+        about,
+        photo,
+        socialLinks,
+        password: hashUserPassword(password),
+      });
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } else {
+      res.status(400).json({
+        success: false,
+        status: 400,
+        message: 'Bad information format',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const userLogin = (req, res, next) => {
