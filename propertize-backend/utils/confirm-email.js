80db09bcdb97fce5a29eeea3c2b417ca1b1815a1
dukelester <dukelester4@gmail.com>
2023-02-02 11:26:50 +0300

    Adeded send user confirmation email
    this function will take the user name, email and confrimation code to validate the user details and confirm the email.

---
+++ propertize-backend/utils/confirm-email.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import * as dotenv from 'dotenv';
+import { createTransport } from 'nodemailer';
+
+dotenv.config();
+
+const sendUser = process.env.EMAIL_HOST_USER;
+const userPass = process.env.EMAIL_HOST_PASSWORD;
+
+const transport = createTransport({
+  service: 'Gmail',
+  auth: {
+    user: sendUser,
+    pass: userPass,
+  },
+});
+
+const sendConfirmationEmail = (name, email, confirmationCode) => {
+  console.log('....checking....sending...');
+  transport.sendMail({
+    from: sendUser,
+    to: email,
+    subject: 'Please confirm your email.',
+    html: `<h1>Email Confirmation</h1>
+        <h2>Hello ${name}</h2>
+        <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+        <a href=http://localhost:5000/api/auth/confirm/${confirmationCode}> Click here</a>
+        </div>`,
+  }).catch((error) => {
+    console.log(error.message);
+  });
+};
+
+export default sendConfirmationEmail;
