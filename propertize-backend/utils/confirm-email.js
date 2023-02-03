c429e8f3572b83c35b0b6d08586ae7b7cc5ed35b
dukelester <dukelester4@gmail.com>
2023-02-03 15:55:52 +0300

    Added the Host to the env file

--- propertize-backend/utils/confirm-email.js
+++ propertize-backend/utils/confirm-email.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const sendUser = process.env.EMAIL_HOST_USER;
 const userPass = process.env.EMAIL_HOST_PASSWORD;
+const host = process.env.HOST;
 
 const transport = createTransport({
   service: 'Gmail',
@@ -24,7 +25,7 @@ const sendConfirmationEmail = (name, email, confirmationCode) => {
     html: `<h1>Email Confirmation</h1>
         <h2>Hello ${name}</h2>
         <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
-        <a href=http://localhost:5000/api/auth/confirm/${confirmationCode}> Click here</a>
+        <a href=/${host}/auth/confirm/${confirmationCode}> Click here</a>
         </div>`,
   }).catch((error) => {
     throw new Error(error);
