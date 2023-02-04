819892694cac2b21d344e162fc81346a6ebcd064
dukelester <dukelester4@gmail.com>
2023-02-04 10:55:27 +0300

    Added the formating on the phone

--- propertize-backend/tests/formatphone.spec.js
+++ propertize-backend/tests/formatphone.spec.js
@@ -4,21 +4,23 @@ describe('formating a phone number', () => {
   describe('format a number to return the desired output', () => {
     it('should return a phone number formatted in kenyan format', () => {
       expect(formatPhoneNumber('0745722340')).toStrictEqual('+254745722340');
+      console.log('testing', formatPhoneNumber('074572230'))
     });
   });
   describe('formatting an invalid phone number', () => {
-    it('should return a phone number formatted in kenyan format', () => {
-      expect(formatPhoneNumber('045722340')).toStrictEqual('invalid phone');
+    it('should return an error', () => {
+      expect(formatPhoneNumber('045722340')).toStrictEqual('Invalid phone number');
+    });
+  });
+  describe('formatting an invalid phone number', () => {
+    it('should return an error', () => {
+      expect(formatPhoneNumber('745722340')).toThrow('Invalid phone number');
+      console.log(formatPhoneNumber('745722340'), 'hererr');
     });
   });
   describe('formatting an invalid phone number', () => {
     it('should return an error for an invalid phone number', () => {
-      expect(formatPhoneNumber('745722340')).toStrictEqual('invalid phone');
-    });
-  });
-  describe('formatting an invalid phone number', () => {
-    it('should return an error for an invalid phone number', () => {
-      expect(formatPhoneNumber('074572238940')).toStrictEqual('invalid phone');
+      expect(formatPhoneNumber('074572238940')).toThrow('Invalid phone number');
     });
   });
 });
--- propertize-backend/utils/formatphone.js
+++ propertize-backend/utils/formatphone.js
@@ -1,8 +1,11 @@
 const formatPhoneNumber = (phoneNumber) => {
   if (phoneNumber.length !== 10 || phoneNumber.at(0) !== '0') {
-    throw new Error();
+    throw new Error('Invalid phone number');
   }
   return `+254${phoneNumber.slice(1)}`;
 };
 
 export default formatPhoneNumber;
+
+
+console.log(formatPhoneNumber('07456782'))
