74ff6f88c80307ae73aa04a5315563be01149680
dukelester <dukelester4@gmail.com>
2023-02-05 13:44:16 +0300

    Added the cors headers to allow the data to be fetched from the api

--- propertize-backend/utils/createServer.js
+++ propertize-backend/utils/createServer.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import agentsRouter from '../routes/agents.js';
 import contactRouter from '../routes/contactus.js';
@@ -12,6 +13,7 @@ import blogRouter from '../routes/blog.js';
 const createServer = () => {
   const app = express();
   app.use(express.json());
+  app.use(cors());
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
   app.use('/api/agents', agentsRouter);
