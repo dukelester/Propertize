a6ea331caa98c15d193fea0c79576a08e4e82cd5
dukelester <dukelester4@gmail.com>
2023-02-07 14:28:01 +0300

    Added the agents and listing for the homepage

--- propertize-front-end/src/components/section-components/agent-v4.js
+++ propertize-front-end/src/components/section-components/agent-v4.js
@@ -1,93 +1,53 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import parse from 'html-react-parser';
+import React from 'react';
 
-class AgentV4 extends Component {
+import useFetch from '../../hooks/useFetch'
+import API_HOST from '../../configs';
 
-    render() {
+const AgentV4 = () => {
+  let publicUrl = process.env.PUBLIC_URL+'/';
+  const { data, loading, error } = useFetch(`${API_HOST}/agents/`);
+  console.log(data);
 
-        let publicUrl = process.env.PUBLIC_URL+'/'
-
-    return <div className="agent-area bg-black pd-top-118 pd-bottom-90">
+  return <div className="agent-area bg-black pd-top-118 pd-bottom-90">
               <div className="container">
                 <div className="section-title style-white text-center">
                   <h6>Meet Our Agent</h6>
                   <h2>Our Best Agent</h2>
                 </div>
                 <div className="row">
-                  <div className="col-lg-3 col-sm-6">
+                
+                  {loading ? ( <h5> Loading data ...</h5>) :
+                  data.slice(0, 8).map((agent) => {
+                    return (
+                      <div className="col-lg-3 col-sm-6" key={agent._id}>
                     <div className="single-agent-wrap style-2 text-center">
                       <div className="thumb">
+                      {agent.photo && 
+                        <img src={agent.photo} alt="img" />
+                      }
+                      {!agent.photo && 
                         <img src={publicUrl+"assets/img/agent/7.png"} alt="img" />
-                      </div> 
+                      }
+                      </div>
                       <div className="details">
-                        <h4>Sandara Mrikon</h4>
-                        <h6>Estate Agent</h6>
+                        <h4>{agent.fullName}</h4>
+                        <h6>{agent.company}</h6>
                         <ul className="social-area style-2">
                           <li><a href="#"><i className="fab fa-facebook-f" aria-hidden="true" /></a></li>
                           <li><a href="#"><i className="fab fa-linkedin-in" aria-hidden="true" /></a></li>
                           <li><a href="#"><i className="fab fa-instagram" aria-hidden="true" /></a></li>
                           <li><a href="#"><i className="fab fa-twitter" aria-hidden="true" /></a></li>
                         </ul>
-                      </div>        
+                      </div>
                     </div>
                   </div>
-                  <div className="col-lg-3 col-sm-6">
-                    <div className="single-agent-wrap style-2 text-center">
-                      <div className="thumb">
-                        <img src={publicUrl+"assets/img/agent/8.png"} alt="img" />
-                      </div> 
-                      <div className="details">
-                        <h4>Sarif Miprut</h4>
-                        <h6>Estate Agent</h6>
-                        <ul className="social-area style-2">
-                          <li><a href="#"><i className="fab fa-facebook-f" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-linkedin-in" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-instagram" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-twitter" aria-hidden="true" /></a></li>
-                        </ul>
-                      </div>        
-                    </div>
-                  </div>
-                  <div className="col-lg-3 col-sm-6">
-                    <div className="single-agent-wrap style-2 text-center">
-                      <div className="thumb">
-                        <img src={publicUrl+"assets/img/agent/9.png"} alt="img" />
-                      </div> 
-                      <div className="details">
-                        <h4>Jordan Smith</h4>
-                        <h6>Estate Agent</h6>
-                        <ul className="social-area style-2">
-                          <li><a href="#"><i className="fab fa-facebook-f" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-linkedin-in" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-instagram" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-twitter" aria-hidden="true" /></a></li>
-                        </ul>
-                      </div>        
-                    </div>
-                  </div>
-                  <div className="col-lg-3 col-sm-6">
-                    <div className="single-agent-wrap style-2 text-center">
-                      <div className="thumb">
-                        <img src={publicUrl+"assets/img/agent/10.png"} alt="img" />
-                      </div> 
-                      <div className="details">
-                        <h4>Jaya Sarif</h4>
-                        <h6>Estate Agent</h6>
-                        <ul className="social-area style-2">
-                          <li><a href="#"><i className="fab fa-facebook-f" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-linkedin-in" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-instagram" aria-hidden="true" /></a></li>
-                          <li><a href="#"><i className="fab fa-twitter" aria-hidden="true" /></a></li>
-                        </ul>
-                      </div>        
-                    </div>
-                  </div>
+                    )})
+                  }
                 </div>
               </div>
             </div>
 
-        }
+        
 }
 
 export default AgentV4
