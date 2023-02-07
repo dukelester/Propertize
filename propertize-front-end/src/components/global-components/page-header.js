52d7f7a7e4b613dc526293caf09dbfc4ced6eeab
dukelester <dukelester4@gmail.com>
2023-02-07 13:21:35 +0300

    Eddited the header and make the breadcramb smaller

--- propertize-front-end/src/components/global-components/page-header.js
+++ propertize-front-end/src/components/global-components/page-header.js
@@ -11,15 +11,11 @@ class Page_header extends Component {
 
         return (
 
-		<div className="breadcrumb-area bg-overlay-2" style={{backgroundImage: 'url("'+publicUrl+'assets/img/other/7.png")'}}>
+		<div className="bg-overlay-2" style={{backgroundImage: 'url("'+publicUrl+'assets/img/other/7.png")'}}>
 		  <div className="container">
 		    <div className="breadcrumb-inner">
 		      <div className="section-title text-center">
-		        <h2 className="page-title">{ HeaderTitle }</h2>
-		        <ul className="page-list">
-		          <li><Link to="/">Home</Link></li>
-		          <li>{ Subheader }</li>
-		        </ul>
+		        
 		      </div>
 		    </div>
 		  </div>
