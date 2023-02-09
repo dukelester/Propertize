8e7de16d57605eebc184fb1b30e3e1310dd4189b
dukelester <dukelester4@gmail.com>
2023-02-09 13:34:57 +0300

    Added the Auth context and the provider

--- propertize-front-end/src/context/AuthContext.js
+++ propertize-front-end/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
     user: JSON.parse(localStorage.getItem('user')) || null,
@@ -35,4 +35,15 @@ const AuthReducer = (state, action) => {
     default:
         return state;
     }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(state.user))
+  }, [state.user]);
+  
+  return <AuthContext.Provider
+  value={ { user: state.user, loading: state.loading, error: state.error, dispatch }}>
+  </AuthContext.Provider>
 }
